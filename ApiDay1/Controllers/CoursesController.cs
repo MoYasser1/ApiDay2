0fda5f4a626a0eb5f27ca7021138bc3651365044
moham <moham@DIVMOASHOR>
2024-11-03 21:54:32 +0200

    Add project files.

---
+++ ApiDay1/Controllers/CoursesController.cs
@@ -0,0 +1,26 @@
+﻿using ApiDay1;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class CoursesController : ControllerBase
+{
+    private readonly List<Course> _courses;
+
+    public CoursesController()
+    {
+        _courses = new List<Course>
+        {
+           new Course { Id = 1, Name = " C# ", Description = "تعلم أساسيات لغة C# لبناء تطبيقات ويب وواجهات مستخدم" },
+            new Course { Id = 2,  Name = "React JS ", Description = "استخدام React لبناء واجهات مستخدم  وتطبيقات  " },
+            new Course { Id = 3,  Name = "SQL Server", Description = "تعلم إدارة قواعد بيانات SQL Server" },
+            new Course { Id = 4,  Name = "Python ", Description = "استخدام Python لتحليل البيانات وتعلم نماذج التعلم الآلي" },
+            new Course { Id = 5,  Name = "ASP.NET Core", Description = "بناء تطبيقات ويب قوية ومؤمنة باستخدام ASP.NET Core" }        };
+    }
+
+    [HttpGet]
+    public IActionResult GetAllCourses()
+    {
+        return Ok(_courses);
+    }
+}
---
+++ ApiDay1/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiDay1
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
